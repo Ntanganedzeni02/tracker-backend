a160a8395ce2bd26aaa8753b1e00bdc1a5fffdda
Ntanga <phidzaglimantanganedzeni@gmail.com>
2025-11-06 12:00:52 +0200

    fix db_url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    'https://tracker.vercel.app/',
+    'https://silulo.vercel.app/',
     'http://localhost:3000'                   // For local testing
   ],
   credentials: true
